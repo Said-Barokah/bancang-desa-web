585f09ca69fb4a02b9c2818931a965d312087bb0
Said-Barokah <yanginiemailnyasaid@gmail.com>
2022-05-17 00:02:19 +0700

    proses pembuatan navbar

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <navbar></navbar>
+    <image-slide></image-slide>
+</template>
+<script>
+    import Navbar from './Components/Home/Navbar.vue';
+    import ImageSlide from './Components/Home/ImageSlide.vue';
+
+    export default {
+        components: {
+            Navbar,
+            ImageSlide
+        },
+
+        data() {
+            return {
+                messages: [{
+
+                }]
+            }
+        }
+    }
+
+</script>
